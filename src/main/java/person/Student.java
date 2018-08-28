94f7a2b1ca34d982343eb1108f0bee89f1dbb962
longhoang94 <longsutls@gmail.com>
2018-08-28 20:53:59 +0700

    add sub-class Stundent

---
+++ src/main/java/person/Student.java
@@ -0,0 +1,50 @@
+package person;
+
+public class Student extends Person {
+    private double fee = 1;
+    private String program = "hello";
+    private int year = 1;
+
+    public Student(double fee, String program, int year) {
+        this.fee = fee;
+        this.program = program;
+        this.year = year;
+    }
+
+    public Student(String name, String address, double fee, String program, int year) {
+        super(name, address);
+        this.fee = fee;
+        this.program = program;
+        this.year = year;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "thằng " + getName() + " đang trú tại: " + getAddress() + " đang học chương trình: " + getProgram()
+                + " trong " + getYear() + ", nợ " + getFee() + " tiền học phí";
+    }
+}
